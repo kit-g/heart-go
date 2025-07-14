12dd365087904a26b03bbc2ea41e951d0dad323a
_kitg <gerasimovkit@gmail.com>
2025-07-14 16:30:18 -0400

    feat: get exercises handler

---
+++ api/internal/handlers/exercises.go
@@ -0,0 +1,40 @@
+package handlers
+
+import (
+	"github.com/gin-gonic/gin"
+	"heart/internal/dbx"
+	"heart/internal/models"
+)
+
+// GetExercises godoc
+//
+//	@Summary		List all exercises
+//	@Description	Returns all exercises in a single page
+//	@Tags			workouts
+//	@Accept			json
+//	@Produce		json
+//	@ID				getExercises
+//	@Success		200	{object}	ExercisesResponse
+//	@Failure		401	{object}	ErrorResponse	"Unauthorized"
+//	@Failure		500	{object}	ErrorResponse	"Server error"
+//	@Router			/exercises [get]
+//	@Security		BearerAuth
+func GetExercises(_ *gin.Context, _ string) (any, error) {
+	var exercises []models.Exercise
+
+	query := dbx.DB.Model(&models.Exercise{})
+
+	if err := query.Find(&exercises).Error; err != nil {
+		return nil, models.NewServerError(err)
+	}
+
+	response := models.ExercisesResponse{
+		Exercises: make([]models.ExerciseOut, len(exercises)),
+	}
+
+	for i, e := range exercises {
+		response.Exercises[i] = models.NewExerciseOut(e)
+	}
+
+	return response, nil
+}
