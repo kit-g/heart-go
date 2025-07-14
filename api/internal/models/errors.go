b5d5f8ae0dbbf67a55b7ba2dfc5902e4660d4498
_kitg <gerasimovkit@gmail.com>
2025-07-14 16:30:09 -0400

    feat: ErrorResponse model

--- api/internal/models/errors.go
+++ api/internal/models/errors.go
@@ -118,3 +118,8 @@ func NewNotFoundError(msg string, err error) *NotFoundError {
 		},
 	}
 }
+
+type ErrorResponse struct {
+	Error string `json:"error" example:"An unexpected error occurred"`
+	Code  string `json:"code" example:"InternalError"`
+} // @name ErrorResponse
