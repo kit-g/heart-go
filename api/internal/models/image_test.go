13170179f4b6123fc8269bb119a813439679cc14
_kitg <gerasimovkit@gmail.com>
2025-07-16 20:58:38 -0400

    test: image.go

---
+++ api/internal/models/image_test.go
@@ -0,0 +1,32 @@
+package models
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestImageDescription(t *testing.T) {
+	t.Run("ImageDescription with all fields", func(t *testing.T) {
+		link := "https://example.com/image.jpg"
+		width := 800
+		height := 600
+
+		img := ImageDescription{
+			Link:   &link,
+			Width:  &width,
+			Height: &height,
+		}
+
+		assert.Equal(t, link, *img.Link)
+		assert.Equal(t, width, *img.Width)
+		assert.Equal(t, height, *img.Height)
+	})
+
+	t.Run("ImageDescription with nil fields", func(t *testing.T) {
+		img := ImageDescription{}
+
+		assert.Nil(t, img.Link)
+		assert.Nil(t, img.Width)
+		assert.Nil(t, img.Height)
+	})
+}
