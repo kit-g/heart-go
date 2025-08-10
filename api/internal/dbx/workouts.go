a641eafb8f1e60245b1edd68d849329a0da4c867
_kitg <gerasimovkit@gmail.com>
2025-08-10 14:33:09 -0400

    fix: imports sorting

--- api/internal/dbx/workouts.go
+++ api/internal/dbx/workouts.go
@@ -3,14 +3,15 @@ package dbx
 import (
 	"context"
 	"errors"
-	"github.com/aws/aws-sdk-go-v2/aws"
-	"github.com/aws/aws-sdk-go-v2/feature/dynamodb/attributevalue"
-	"github.com/aws/aws-sdk-go-v2/service/dynamodb"
-	"github.com/aws/aws-sdk-go-v2/service/dynamodb/types"
 	"heart/internal/awsx"
 	"heart/internal/config"
 	"heart/internal/models"
 	"strings"
+
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/feature/dynamodb/attributevalue"
+	"github.com/aws/aws-sdk-go-v2/service/dynamodb"
+	"github.com/aws/aws-sdk-go-v2/service/dynamodb/types"
 )
 
 func GetWorkout(ctx context.Context, userId string, workoutId string) (*models.Workout, error) {
