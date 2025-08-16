18cea2ff79f62defd4d0d0411179642599c015b8
_kitg <gerasimovkit@gmail.com>
2025-08-16 14:25:06 -0400

    feat: introduce DynamoDbAPI interface for dynamodb client abstraction in aws package

--- api/internal/awsx/aws.go
+++ api/internal/awsx/aws.go
@@ -18,8 +18,16 @@ import (
 	"github.com/aws/aws-sdk-go-v2/service/sns"
 )
 
+type DynamoDbAPI interface {
+	GetItem(ctx context.Context, params *dynamodb.GetItemInput, optFns ...func(*dynamodb.Options)) (*dynamodb.GetItemOutput, error)
+	PutItem(ctx context.Context, params *dynamodb.PutItemInput, optFns ...func(*dynamodb.Options)) (*dynamodb.PutItemOutput, error)
+	UpdateItem(ctx context.Context, params *dynamodb.UpdateItemInput, optFns ...func(*dynamodb.Options)) (*dynamodb.UpdateItemOutput, error)
+	DeleteItem(ctx context.Context, params *dynamodb.DeleteItemInput, optFns ...func(*dynamodb.Options)) (*dynamodb.DeleteItemOutput, error)
+	Query(ctx context.Context, params *dynamodb.QueryInput, optFns ...func(*dynamodb.Options)) (*dynamodb.QueryOutput, error)
+}
+
 var (
-	Db       *dynamodb.Client
+	Db       DynamoDbAPI
 	Env      env.AwsConfig
 	events   *scheduler.Client
 	S3       *s3.Client
